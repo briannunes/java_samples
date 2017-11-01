ff3f320f8f96ff4f57ae002f1bd5bee08400f514
briannunes <32107502+briannunes@users.noreply.github.com>
2017-11-01 11:53:45 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,20 @@
+/**
+ * Hello World.
+ *
+ * @author Brian Nunes
+ * @version 1
+ */
+ 
+class hello
+{
+    public void go ()
+    {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args)
+    {
+        hello hi = new hello();
+        hi.go();
+    }
+}
